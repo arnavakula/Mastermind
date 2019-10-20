102a3d7e33bffc3ed085793652769b6c7e5779b7
Arnav Akula <akula.arnav@gmail.com>
2019-10-19 18:12:19 -0700

    Get valid user input

---
+++ Mastermind/src/app/App.java
@@ -0,0 +1,70 @@
+package app;
+
+import java.util.Scanner;
+import app.Constants;
+
+public class App {
+
+    public static void main(String[] args) { 
+        Scanner scan = new Scanner(System.in);
+        String input = getUserInput(scan);
+        System.out.println(isValidInput(input));
+       
+    } 
+
+    public static String getUserInput(Scanner scan){
+        String input;
+
+        System.out.println("These are the valid options: red(r), green, blue(b), yellow(y), silver(s), orange(o), purple(p), white(w)");
+        while (true){
+            System.out.print("Please enter your input: ");
+            input = scan.nextLine();
+            input = input.toLowerCase();
+            if(isValidInput(input)){
+                return input;
+            }
+        }
+    }
+
+    public static boolean isValidInput(String s){
+        char[] colors = {'r', 'g', 'b', 'y', 's', 'o', 'p', 'w'};
+        boolean isValid = false;
+        
+        if(s.length() != Constants.SEQUENCE_LENGTH || hasDuplicates(s)){
+            return false;
+        }
+
+        for(int i = 0; i < 4; i++){
+            isValid = false;
+
+            for(int j = 0; j < colors.length; j++){
+                if(s.charAt(i) == colors[j]){
+                    isValid = true;
+                    break;
+                }
+            }
+            // --> break enters here
+            if(!isValid){ return false; }
+            if(i == 4){ return isValid; }
+        }
+
+        return true;
+    }
+
+    public static boolean hasDuplicates(String s){
+        for(int i = 0; i < s.length(); i++){
+            if(i == 4){ break; } //for index errors - C++ formatting technique lol
+            for(int j = i + 1; j < s.length(); j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+}
---
+++ Mastermind/src/app/Constants.java
@@ -0,0 +1,5 @@
+package app;
+
+public class Constants{
+    public static int SEQUENCE_LENGTH = 4;
+}
